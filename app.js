b21eaed5b955b5510e4c4e10fbb39335d5c47a24
vinod540 <77802816+vinod540@users.noreply.github.com>
2021-03-24 16:10:26 -0500

    added mydata router files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var PORT = process.env.PORT || 5000;
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,7 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var mydatarouter = require('./routes/mydata');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata',mydatarouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
